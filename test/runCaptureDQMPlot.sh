d4683681009a53008d568270ad5e92b8d0e5f4df
maria <cepeda@cern.ch>
2014-12-04 05:18:07 -0600

    Add link to capture log

--- test/runCaptureDQMPlot.sh
+++ test/runCaptureDQMPlot.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-cmsRun CTP7ToDigi_cfg.py >& logcapture
+cmsRun CTP7ToDigi_cfg.py >& capture.log
 
 cmsRun CTP7DQM_cfg.py   >& dqm.log 
 root -b -q fastplotter.C >& plots.log 
 
 foldername=$(date +%Y%m%d_%H%M%S)
 mkdir -p "$foldername" 
-mv *png *log  "$foldername"
+mv *png *log "$foldername"
 cp index.html "$foldername"
 
 mv CTP7DQM.root  "$foldername"
